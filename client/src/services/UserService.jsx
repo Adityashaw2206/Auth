a89995035806974932b41d0b54a8795a22c333a8
Adityashaw2206 <shawaditya137@gmail.com>
2026-04-19 12:57:52 +0530

    Updated api

--- client/src/services/UserService.jsx
+++ client/src/services/UserService.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/user";
+// const API = "http://localhost:5000/api/user";
+const API = import.meta.env.VITE_API_URL;
 
 
 axios.defaults.withCredentials = true;
